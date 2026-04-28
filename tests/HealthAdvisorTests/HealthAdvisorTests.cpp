dcd610de7374215181b29e0fd6954404d0d492bb
User <User@DESKTOP-3DASERP>
2026-04-28 17:48:46 +0400

    Добавьте файлы проекта.

---
+++ tests/HealthAdvisorTests/HealthAdvisorTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HealthAdvisorTests
+{
+	TEST_CLASS(HealthAdvisorTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
